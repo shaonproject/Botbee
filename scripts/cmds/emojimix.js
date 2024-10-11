ee195b58ade2763efddb8d452a4681db061669a2
shaonproject <153928138+shaonproject@users.noreply.github.com>
2024-10-11 16:01:22 +0600

    Update emojimix.js

--- scripts/cmds/emojimix.js
+++ scripts/cmds/emojimix.js
@@ -25,11 +25,11 @@ const content = args.join(" ").split("|").map(item => item = item.trim());
 let emoji1 = content[0]
 let emoji2 = content [1]
 if (!args[0])
-  return api.sendMessage("Wrong format!\nUse "+global.config.PREFIX+this.config.name+" "+this.config.usages, event.threadID, event.messageID);
+  return api.message.reply("Wrong format!\nUse "+global.config.PREFIX+this.config.name+" "+this.config.usages, event.threadID, event.messageID);
 
- var callback = () => api.sendMessage({body:``,attachment: fs.createReadStream(__dirname + "/cache/biden.png")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/biden.png"),event.messageID);
- return request(encodeURI(`${n}/nayan/emojimix?emoji1=${emoji1}&emoji2=${emoji2}`)).pipe(fs.createWriteStream(__dirname+'/cache/biden.png')).on('close',() => callback()); 
-} catch (err){
-return api.sendMessage("Can't mix "+emoji1+" and "+emoji2, event.threadID, event.messageID)
+ var callback = () => api.message.reply({body:``,attachment: fs.message.stream(__dirname + "/cache/biden.png")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/biden.png"),event.messageID);
+ return request(encodeURI(`${n}/nayan/emojimix?emoji1=${emoji1}&emoji2=${emoji2}`)).pipe(fs.message.stream(__dirname+'/cache/biden.png')).on('close',() => callback()); 
+} catch (err){ 
+  return api.message.reply("Can't mix "+emoji1+" and "+emoji2, event.threadID, event.messageID)
 }   
 }}
