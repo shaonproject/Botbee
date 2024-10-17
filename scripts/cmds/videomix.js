8b0258459b02249678f47c69f76bf53a73db1f87
shaonproject <153928138+shaonproject@users.noreply.github.com>
2024-10-17 08:15:52 +0600

    Update videomix.js

--- scripts/cmds/videomix.js
+++ scripts/cmds/videomix.js
@@ -10,7 +10,7 @@ module.exports.config = {
   cooldowns: 30,
 };
 
-module.exports.run = async function({ api, event }) {
+module.exports.run = async function({ api, message }) {
   const axios = require('axios');
   const request = require('request');
   const fs = require("fs");
