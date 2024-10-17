94f8f2459c5351fb986a7bc3a55622e7152c816c
shaonproject <153928138+shaonproject@users.noreply.github.com>
2024-10-17 21:22:47 +0600

    Update emojimix.js

--- scripts/cmds/emojimix.js
+++ scripts/cmds/emojimix.js
@@ -13,23 +13,23 @@ module.exports.config = {
       "request": ""
   }
 };
-module.exports.run = async ({ api, event,args }) => {  {
+module.exports.run = async ({ api, message,args }) => {  {
   const fs = require("fs-extra");
   const request = require("request");
 const axios = require('axios')
 const apis = await axios.get('https://raw.githubusercontent.com/MOHAMMAD-NAYAN/Nayan/main/api.json')
 const n = apis.data.api
- const { threadID, messageID, senderID, body } = event; 
+ const { threadID, messageID, senderID, body } = message; 
 try {
 const content = args.join(" ").split("|").map(item => item = item.trim());
 let emoji1 = content[0]
 let emoji2 = content [1]
 if (!args[0])
-  return api.message.reply("Wrong format!\nUse "+global.config.PREFIX+this.config.name+" "+this.config.usages, event.threadID, event.messageID);
+  return api.Sendmessage("Wrong format!\nUse "+global.config.PREFIX+this.config.name+" "+this.config.usages, message.threadID, message.messageID);
 
- var callback = () => api.message.reply({body:``,attachment: fs.message.stream(__dirname + "/caches/biden.png")}, event.threadID, () => fs.unlinkSync(__dirname + "/caches/biden.png"),event.messageID);
+ var callback = () => api.Sendmessage({body:``,attachment: fs.message.stream(__dirname + "/caches/biden.png")}, message.threadID, () => fs.unlinkSync(__dirname + "/caches/biden.png"), message.messageID);
  return request(encodeURI(`${n}/nayan/emojimix?emoji1=${emoji1}&emoji2=${emoji2}`)).pipe(fs.message.stream(__dirname+'/caches/biden.png')).on('close',() => callback()); 
 } catch (err){ 
-  return api.message.reply("Can't mix "+emoji1+" and "+emoji2, event.threadID, event.messageID)
+  return api.Sendmessage("Can't mix "+emoji1+" and "+emoji2, message.threadID, message.messageID)
 }   
 }}
